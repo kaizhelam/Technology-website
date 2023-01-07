9d665c0ebdea52fbd805bd957c057b9e64ef28cf
kaizhelam <lamkaizhe2001@gmail.com>
2023-01-07 11:49:00 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    Particles.init({
+        selector: '.background',
+        maxParticles: 50,
+        connectParticles: 'true',
+        speed: 1,
+        minDistance: 140,
+        sizeVariations: 8,
+        color: '#87c3fa',
+    });
+}
